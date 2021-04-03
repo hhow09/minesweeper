dff2e9e90c67696af366d4bfd484999cd8623123
howardhuang <howardhuang@rayark.com>
2021-04-03 21:34:44 +0800

    feat: adjust configuration and layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import Board from "component/Board";
 import { useState } from "react";
-const pageStyle = { display: "flex", flexDirection: "column", alignItems: "center" };
+import githubLogo from "assests/gitHub-logo.png";
+const pageStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  minHeight: "100vh",
+};
 
-const configStyle = { display: "flex", flexDirection: "column", marginBottom: "20px" };
+const configStyle = {
+  display: "flex",
+  flexDirection: "column",
+  marginBottom: "20px",
+  border: "2px solid black",
+  padding: "20px",
+};
 const labelStyle = { display: "flex", justifyContent: "space-between", marginBottom: "5px" };
 
 const Page = ({ title, children }) => (
@@ -15,10 +27,14 @@ const Page = ({ title, children }) => (
 function App() {
   const [boardWidth, setBoardWidth] = useState(10);
   const [boardHeight, setBoardHeight] = useState(10);
+  const [bombProbability, setBombProbability] = useState(0.2);
   const [showLog, setShowLog] = useState(false);
 
   const [started, setStarted] = useState(false);
   const [round, setRound] = useState(1);
+
+  const validBoardConfig =
+    boardWidth > 0 && Number.isInteger(boardHeight) && boardHeight > 0 && bombProbability > 0;
 
   const handleEndGame = (win) => {
     if (win) alert("You win");
@@ -26,9 +42,20 @@ function App() {
     setStarted(false);
   };
   return (
-    <div>
+    <>
       <Page>
-        <div style={configStyle}>
+        <header>
+          <h2>
+            React Minesweeper
+            <a href="https://github.com/hhow09/minesweeper" target="_blank" rel="noreferrer">
+              <img src={githubLogo} width="20px" style={{ marginLeft: "15px" }} alt="github" />
+            </a>
+          </h2>
+        </header>
+        <section style={configStyle}>
+          <label style={labelStyle}>
+            <strong>Configuration</strong>
+          </label>
           <label style={labelStyle}>
             Board Width
             <input
@@ -38,7 +65,7 @@ function App() {
               disabled={started}
               min={1}
               onChange={(e) => {
-                setBoardWidth(parseInt(e.target.value));
+                setBoardWidth(parseInt(e.target.value) || 0);
               }}
             />
           </label>
@@ -50,7 +77,20 @@ function App() {
               disabled={started}
               min={1}
               onChange={(e) => {
-                setBoardHeight(parseInt(e.target.value));
+                setBoardHeight(parseInt(e.target.value) || 0);
+              }}
+            />
+          </label>
+          <label style={labelStyle}>
+            Bomb Probability
+            <input
+              value={bombProbability}
+              type="number"
+              disabled={started}
+              min={0.1}
+              max={1}
+              onChange={(e) => {
+                setBombProbability(parseFloat(e.target.value) || 0);
               }}
             />
           </label>
@@ -64,6 +104,7 @@ function App() {
               }}
             />
           </label>
+          <label style={labelStyle}> (check Devtools console panel)</label>
           <button
             onClick={() => {
               if (started) {
@@ -72,20 +113,23 @@ function App() {
                 setStarted(true);
                 setRound((prev) => ++prev);
               }
-            }}>
+            }}
+            disabled={!validBoardConfig}>
             {started ? "STOP" : "START"}
           </button>
-        </div>
-        <Board
-          key={round}
-          width={boardWidth}
-          height={boardHeight}
-          endGameCallback={handleEndGame}
-          disabled={!started}
-          showLog={showLog}
-        />
+        </section>
+        <section>
+          <Board
+            key={round}
+            width={boardWidth}
+            height={boardHeight}
+            endGameCallback={handleEndGame}
+            disabled={!started}
+            showLog={showLog}
+          />
+        </section>
       </Page>
-    </div>
+    </>
   );
 }
 
