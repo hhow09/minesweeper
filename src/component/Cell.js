fbe56236295011dccbf0e1c7a065de30aa59de38
hhow09 <hhow09@gmail.com>
2021-04-03 15:29:19 +0800

    feat: adjust layout cell

--- src/component/Cell.js
+++ src/component/Cell.js
@@ -1,9 +1,20 @@
 import "assests/cell.css";
+import bombPic from "assests/bomb.png";
+import flagPic from "assests/flag.png";
+
 import PropTypes from "prop-types";
 
-const Cell = ({ opened, isBomb, adjBombNum, onClick }) => (
-  <div className={`cell ${opened && "opened"}`} onClick={onClick}>
-    {opened ? (isBomb ? "B" : adjBombNum) : null}
+const Cell = ({ opened, isBomb, adjBombNum, onClick, disabled, flagged }) => (
+  <div className={`cell ${opened && "opened"}`} onClick={!disabled ? onClick : () => null}>
+    {opened ? (
+      isBomb ? (
+        <img src={bombPic} width="100%" />
+      ) : (
+        adjBombNum > 0 && <span className={`bombNum_${adjBombNum}`}>{adjBombNum}</span>
+      )
+    ) : flagged ? (
+      <img src={flagPic} width="100%" />
+    ) : null}
   </div>
 );
 
@@ -12,6 +23,7 @@ Cell.propTypes = {
   isBomb: PropTypes.bool.isRequired,
   adjBombNum: PropTypes.number.isRequired,
   onClick: PropTypes.func.isRequired,
+  flagged: PropTypes.bool,
 };
 
 export default Cell;
