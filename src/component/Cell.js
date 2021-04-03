ffcc98be08aac63882506ce35bb1bfa6c83cc97a
hhow09 <hhow09@gmail.com>
2021-04-03 14:40:28 +0800

    feat: proptypes

--- src/component/Cell.js
+++ src/component/Cell.js
@@ -1,8 +1,17 @@
 import "assests/cell.css";
+import PropTypes from "prop-types";
+
 const Cell = ({ opened, isBomb, adjBombNum, onClick }) => (
   <div className={`cell ${opened && "opened"}`} onClick={onClick}>
     {opened ? (isBomb ? "B" : adjBombNum) : null}
   </div>
 );
+
+Cell.propTypes = {
+  opened: PropTypes.bool.isRequired,
+  isBomb: PropTypes.bool.isRequired,
+  adjBombNum: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Cell;
